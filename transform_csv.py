66e6b1ca3f3aac58b44bba7afb97bdbf32d4e8ed
likeinlife <likeinlife@outlook.com>
2023-12-19 22:40:13 +0300

    fix(transform_csv.py): relative import

--- transform_csv.py
+++ transform_csv.py
@@ -1,7 +1,7 @@
 import csv
 from pathlib import Path
 
-from .config import CYAN_FILENAME, CYAN_TRANSFORMED_FILENAME
+from config import CYAN_FILENAME, CYAN_TRANSFORMED_FILENAME
 
 
 def clear_room_count(room_count_string: str | None) -> int | None:
@@ -29,12 +29,15 @@ def clear_price(price_string: str | None) -> int | None:
     return int(price_string)
 
 
-def clear_house(house_string: str | None) -> str | None:
-    if not house_string:
-        return None
-    if "/" in house_string and ", " in house_string:
-        return house_string.split(", ")[-1]
-    return house_string
+def clear_roof_and_house(
+    roof_and_house_string: str | None
+) -> tuple[str | None, str | None]:
+    if not roof_and_house_string:
+        return (None, None)
+    if "," not in roof_and_house_string:
+        return (roof_and_house_string, None)
+    splitted = roof_and_house_string.split(", ")
+    return (splitted[0], splitted[1])
 
 
 def reformat_csv(old_path: Path, new_path: Path):
@@ -43,17 +46,28 @@ def reformat_csv(old_path: Path, new_path: Path):
         open(new_path, "w", encoding="UTF-8") as new_file,
     ):
         reader = csv.DictReader(old_file, delimiter=";")
-        field_names = ["id", "room_count", "area", "address", "price", "house", "link"]
+        field_names = [
+            "id",
+            "floor",
+            "house",
+            "address",
+            "room_count",
+            "area",
+            "price",
+            "link",
+        ]
         writer = csv.DictWriter(new_file, fieldnames=field_names, delimiter=";")
         writer.writeheader()
         for i in reader:
+            floor, house = clear_roof_and_house(roof_and_house_string=i["Дом"])
             ad_info = {
                 "id": i["\ufeffID"],
                 "room_count": clear_room_count(i["Количество комнат"]),
                 "area": clear_area(i["Площадь, м2"]),
                 "address": i["Адрес"],
                 "price": clear_price(i["Цена"]),
-                "house": clear_house(i["Дом"]),
+                "house": house,
+                "floor": floor,
                 "link": i["Ссылка на объявление"],
             }
             writer.writerow(ad_info)
